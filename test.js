90100db986a3d71dcca7d7e99450afeda27f4987
cap-Bernardito <cap-Bernardito@yandex.ru>
2020-04-16 12:31:43 +0300

    test

--- test.js
+++ test.js
@@ -1,11 +1,62 @@
-const { spawn: spawnProcess } = require("child_process");
+const { spawn: spawnProcess, execSync } = require("child_process");
+const readline = require("readline");
 
-let devServerProcess = spawnProcess(
-    // ".\\node_modules\\.bin\\webpack-dev-server", [],
-    "./node_modules/.bin/webpack-dev-server", [],
-    { shell: true, stdio: "inherit" });
+class SpawnProcess {
+  init() {
+    throw new Error("SpawnProcess.init should be implemeted");
+  }
 
-setTimeout(_ => {
-    console.log("IS KILLED: ", devServerProcess.kill())
-    console.log("Trying to kill webpack-dev-server...");
+  kill() {
+    throw new Error("SpawnProcess.kill should be implemeted");
+  }
+}
+
+class WindowsProcess extends SpawnProcess {
+  init() {
+    this.process = spawnProcess(
+      ".\\node_modules\\.bin\\webpack-dev-server",
+      [],
+      {
+        shell: true,
+        stdio: "inherit",
+      }
+    );
+
+    this.readline = readline.createInterface({
+      input: process.stdin,
+    });
+  }
+
+  kill() {
+    this.readline.close();
+    execSync(`taskkill /pid ${this.process.pid} /f /t`);
+  }
+}
+
+class UnixProcess extends SpawnProcess {
+  init() {
+    this.process = spawnProcess("./node_modules/.bin/webpack-dev-server", [], {
+      shell: true,
+      stdio: [0, 1, 2, "ipc"],
+    });
+  }
+
+  kill() {
+    this.process.send("webpack-dev-server-stop");
+  }
+}
+
+function createProcess() {
+  if (/^win/.test(process.platform)) {
+    return new WindowsProcess();
+  } else {
+    return new UnixProcess();
+  }
+}
+
+let devServerProcess = createProcess();
+devServerProcess.init();
+
+setTimeout((_) => {
+  devServerProcess.kill();
 }, 5000);
